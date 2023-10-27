d96fbacb9b9554384a9416556025181e3fa11307
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-27 10:03:02 -0400

    created hamster class

---
+++ classesAndJsreview.js
@@ -0,0 +1,29 @@
+// =================================
+// Creating Classes & Factories
+// =================================
+// Hamster
+// attributes:
+// owner - string, initially set as an empty string
+// name - string, set the name from parameter in constructor method
+// price - integer, set as 15
+// methods:
+// wheelRun() - log "squeak squeak"
+// eatFood() - log "nibble nibble"
+// getPrice() - return the price
+
+class Hamster {
+    constructor(name){
+        this.name = name    
+        this.owner = ""
+        this.price = 15
+    }
+    wheelRun(){
+        console.log('squeak squeak')
+      }
+      eatFood(){
+        console.log('nibble nibble')
+      }
+      getPrice(){
+        return this.price
+      }
+}
